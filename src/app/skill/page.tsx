04b2492a0ca0ee0b88c4f07d4622a06659d6bc26
kms <cpt4567@gmail.com>
2023-11-17 07:16:00 +0900

    svg 수정

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -3,7 +3,6 @@ export default function Page() {
     <div className="flex flex-row justify-between h-full" style={{ background: '#1E1E1E' }}>
       <div className="flex flex-col h-full" style={{ background: '#333333', width: '5%' }}>
         <button>1</button>
-
         <button>2</button>
         <button>3</button>
         <button>4</button>
