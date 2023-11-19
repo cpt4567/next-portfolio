f13020a2ceeb1faaa81e14fc28336bf685154129
kms <cpt4567@gmail.com>
2023-11-19 20:23:53 +0900

    top

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -2,7 +2,13 @@ export default function Page() {
   return (
     <div className="flex flex-row justify-between h-full">
       <div className="hashtag-container">
-        <div className="hashtag-line"></div>
+        <div className="hashtag-line">
+          <button># React</button>
+          <button># React Naitve</button>
+          <button># Next js</button>
+          <button># java script</button>
+          <button># type script</button>
+        </div>
         <div className="hashtag-line"></div>
         <div className="hashtag-line"></div>
       </div>
