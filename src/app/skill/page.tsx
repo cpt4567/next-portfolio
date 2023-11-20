e8dfe852cfcb7913684c2562627b4250a41ba143
kms <cpt4567@gmail.com>
2023-11-20 22:19:09 +0900

    as

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -1,16 +1,26 @@
 import Button from '@/components/button/button';
 
 export default function Page() {
+  let index = 0;
   return (
-    <div className="flex flex-col justify-between h-full">
-      <div className="h-1/2 section">asdasd</div>
+    <div className="flex flex-col justify-around h-full">
+      <div className=" section" style={{ height: '80%' }}>
+        {/*       <img
+          className="w-full"
+          src="https://v1.tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+        /> */}
 
-      <div className="flex flex-row justify-around h-full">
+        <div className="font-bold text-xl mb-2">React</div>
+        <p className="text-gray-700 text-base">내용</p>
+      </div>
+
+      <div className="flex flex-row justify-around ">
+        <Button># java script</Button>
+        <Button># type script</Button>
         <Button># React</Button>
         <Button># React Naitve</Button>
         <Button># Next js</Button>
-        <Button># java script</Button>
-        <Button># type script</Button>
       </div>
     </div>
   );
